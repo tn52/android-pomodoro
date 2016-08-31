10e49f359ba1b08d107b896cd296d60d0fee9328
tzekang <tn52@duke.edu>
2016-08-31 01:58:40 -0400

    pause functionality added

--- app/src/main/java/com/example/merliora/pomodorotimer/MainActivity.java
+++ app/src/main/java/com/example/merliora/pomodorotimer/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.merliora.pomodorotimer;
 
-import android.os.CountDownTimer;
+//import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean isPaused = true;
 
 
-    CountDownTimer countDownTimer = new CountDownTimer(30000, 1000){
+    CountDownTimerPausable countDownTimer = new CountDownTimerPausable(30000, 1000){
         @Override
         public void onTick(long millisUntilFinished) {
             TextView countDownText = (TextView)findViewById(R.id.countdownTextView);
@@ -41,29 +41,36 @@ public class MainActivity extends AppCompatActivity {
 
         Button startButton = (Button)findViewById(R.id.startButton);
 
-
-
-
         startButton.setOnClickListener(
                 new Button.OnClickListener(){
                     public void onClick(View v){
-                        TextView countDownText = (TextView)findViewById(R.id.countdownTextView);
                         countDownTimer.start();
                     }
                 }
         );
 
 
-        Button resetButton = (Button)findViewById(R.id.pauseButton);
+        Button pauseButton = (Button)findViewById(R.id.pauseButton);
+
+        pauseButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        countDownTimer.pause();
+                    }
+                }
+        );
+
+
+        Button resetButton = (Button)findViewById(R.id.resetButton);
 
         resetButton.setOnClickListener(
                 new Button.OnClickListener(){
                     public void onClick(View v){
-                        TextView countDownText = (TextView)findViewById(R.id.countdownTextView);
-                        countDownTimer.cancel();
+
                     }
                 }
         );
+
 
     }
 
